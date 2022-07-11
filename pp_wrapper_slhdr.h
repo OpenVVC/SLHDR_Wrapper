5cc9f6647f0b88a2d2ba22e0871352ef22460389
Pierre-Loup <pcabarat@insa-rennes.fr>
2022-07-11 12:01:53 +0200

    Add declspec export attribute to wrapper functions

--- pp_wrapper_slhdr.h
+++ pp_wrapper_slhdr.h
@@ -1,23 +1,29 @@
 #ifndef SLHDR_H
 #define SLHDR_H
 
+#ifdef _MSC_VER
+#define export_attribute __declspec(dllexport)
+#else
+#define export_attribute
+#endif
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-void
+export_attribute void
 pp_slhdr_no_filter(void* slhdr_context, int16_t** sdr_pic, int16_t** hdr_pic, 
                 uint8_t* SEIPayload, int pic_width, int pic_height);
 
-void 
+export_attribute void
 pp_sdr_to_hdr(void* slhdr_context, int16_t** sdr_pic, int16_t** hdr_pic, 
                 uint8_t* SEIPayload, int pic_width, int pic_height);
 
-void 
+export_attribute void
 pp_init_slhdr_lib(void** pslhdr_context);
 
-void 
+export_attribute void
 pp_uninit_slhdr_lib(void* slhdr_context);
 
 #ifdef __cplusplus
